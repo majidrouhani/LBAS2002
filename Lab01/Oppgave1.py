c09e87d185da38fb5efb5f0623ffd6eeaf1eeb02
Majid Rouhani <majid.rouhani@ntnu.no>
2020-08-18 12:33:27 +0200

    first version

---
+++ Lab01/Oppgave1.py
@@ -0,0 +1,12 @@
+#Oppgave 1a)
+a = 2
+b = 3
+
+#Oppgave 1b)
+c = a + b
+
+#Oppgave 1c)
+d = c + b
+
+#Oppgave 1d)
+print(d)
---
+++ Lab01/Oppgave2.py
@@ -0,0 +1,8 @@
+#Oppgave 2a)
+a = float(input("Oppgi a: "))
+b = float(input("Oppgi b: "))
+
+#Oppgave 2b)
+c = a + b
+
+print(c)
