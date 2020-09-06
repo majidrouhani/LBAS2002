44c44d94a0af288867727262005498d336ef12cf
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2020-09-06 11:55:35 +0200

    first version

---
+++ Lab03/egendefinert_funksjon.py
@@ -0,0 +1,17 @@
+
+
+#Funksjon for å beregne areal til en sirkel
+# formel: areal=pi*r*r
+
+def beregnArealSirkel(r):
+    areal = 3.14*r*r
+    return areal
+
+radius = float(input("Oppgi radius til sirkel: "))
+
+areal = beregnArealSirkel(radius)
+
+print("Arealet til sirkelen med radius ", radius, "er:",areal)
+
+#eller
+print("Arealet til sirkelen med radius {0} er: {1}".format(radius,areal))
