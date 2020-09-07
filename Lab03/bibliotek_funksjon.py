84ba55783fd22e18c3d2006f8df72d3e1b1edb54
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2020-09-07 08:07:24 +0200

    first

---
+++ Lab03/bibliotek_funksjon.py
@@ -0,0 +1,17 @@
+import math as m
+
+#Funksjon for å beregne areal til en sirkel
+# formel: areal=pi*r*r
+
+def beregnArealSirkel(r):
+    areal = m.pi*m.sqrt(r)
+    return areal
+
+radius = float(input("Oppgi radius til sirkel: "))
+
+areal = beregnArealSirkel(radius)
+
+print(f"Arealet til sirkelen med radius {radius} er: {areal:0.3f}")
+
+#eller
+print("Arealet til sirkelen med radius {0} er: {1:0.3f}".format(radius,areal))
---
+++ Lab03/if_eksempel.py
@@ -0,0 +1,6 @@
+import math as m
+
+alder=int(input("Hvor gammel er du?"))
+
+if alder<0 or alder>99:
+    print("Du har oppgitt ugyldig alder!")
