11a3e694f2823683652c9bdcf14f8bec2e7844e4
Majid Rouhani <majid.rouhani@ntnu.no>
2021-09-19 08:17:31 +0200

    opd

---
+++ sandbox/oppg2.py
@@ -0,0 +1,8 @@
+# Oppgave 3
+# a) Utvid programmet fra oppgave 2: I istedenfor å oppgi faste verdier til a og b, la brukeren taste inn
+# verdien når programmet starter (bruk input kommandoen)
+a = input("Skriv inn verdien for variabel a")
+b = input("Skriv inn verdien for variabel b")
+#b) Legg sammen verdien av a og b i en ny variabel c og skriv ut verdien
+c = a + b
+print(c)
